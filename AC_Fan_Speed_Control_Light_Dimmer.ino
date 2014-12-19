cd4c32eaa5306bb3ff2690ec2781ebc6f3443754
tangophi <gopinath.marappan@gmail.com>
2014-12-20 00:13:13 +0530

    version 2 - using capacitors and triacs
    Capacitors taken from a off the shelf fan regulator have been used to
    vary the speed of the fan.  Using zero detection and firing the triac in
    version 1 was causing noise in the fan as the current increases from
    zero immediately.   hence, capacitors are used.   On the input side,
    there are two relays - one for light and the other for fan.  If Arduino
    is powered off, then the NC ports of the relays are connected and hence
    wall outlet switches will work normally.  When the Arduinos is powered
    on, then these relays are active.  From the light relay, the NO port is
    connected to a triac which can be trigged to switch on the light.  The
    NO port of the fan relay is connected to three capacitors.  There are
    also four triacs, one connected directly to the output and other through
    the capacitors.  Based on how many triacs are triggered, the fan of the
    speed varies.  But if only the triac connected to the output directly is
    fired, then the fan rotates at max speed.   There is one 4 digit 7
    segment display that displays the temperature reading from a Dallas
    18B20 sensor and another one segment display that display fan speed, 0 -
    6.  The temperature is periodically checked and if it goes below 22
    degrees Celsius, the fan is switched off.

--- AC_Fan_Speed_Control_Light_Dimmer.ino
+++ AC_Fan_Speed_Control_Light_Dimmer.ino
@@ -1,34 +1,25 @@
 #include <IRremote.h>
-#include <dht11.h>
 #include  <TimerOne.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-int PIN_IR_RECEIVER = 8; 
+#define PIN_LIGHT_RELAY        2
+#define PIN_FAN_RELAY          3
+
+#define PIN_LIGHT_TRIAC        A0
+#define PIN_FAN_TRIAC_1        A1
+#define PIN_FAN_TRIAC_2        A2
+#define PIN_FAN_TRIAC_3        A3
+#define PIN_FAN_TRIAC_4        A4
+
+#define PIN_IR_RECEIVER        8
+
 IRrecv irrecv(PIN_IR_RECEIVER);
 decode_results results;
 
 volatile int i=0;               // Variable to use as a counter
 volatile boolean zero_cross=0;  // Boolean to store a "switch" to tell us if we have crossed zero
-int AC_pin = 3;                 // Output to Opto Triac
 int fan_speed = 0;              // Fan speed (0-9)  0 = off, 9 = full speed
-int fan_speed_trigger = 0;      // Fan speed (0-9)  0 = off, 9 = full speed
-
-// This is the delay-per-brightness step in microseconds.
-// It is calculated based on the frequency of your voltage supply (50Hz or 60Hz)
-// and the number of brightness steps you want. 
-// 
-// The only tricky part is that the chopper circuit chops the AC wave twice per
-// cycle, once on the positive half and once at the negative half. This meeans
-// the chopping happens at 120Hz for a 60Hz supply or 100Hz for a 50Hz supply. 
-
-// To calculate freqStep you divide the length of one full half-wave of the power
-// cycle (in microseconds) by the number of brightness steps. 
-//
-// (1000000 uS / 100 Hz) / 9 brightness steps = 1111 uS / brightness step
-//
-// 1000000 us / 100 Hz = 10000 uS, length of one half-wave.
-int freqStep = 1111;    
 
 const int digitPins[4] = {4,5,6,7};  //4 common anode pins of the display
 const int clockPin = 11;             //74HC595 Pin 11 
@@ -62,29 +53,6 @@ boolean bAutoMode = true;
 OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-
-void zero_cross_detect() {    
-    zero_cross = true;               // set the boolean to true to tell our dimming function that a zero cross has occured
-    i=0;
-   digitalWrite(AC_pin, LOW);       // turn off TRIAC (and AC)
-}                                 
-
-// Turn on the TRIAC at the appropriate time
-void dim_check() {                   
-    if(zero_cross == true)
-    {              
-        if(i > (9-fan_speed_trigger))
-        {                      
-            digitalWrite(AC_pin, HIGH); // turn on fan       
-            i=0;                        // reset time step counter                         
-            zero_cross = false;         // reset zero cross detection
-        } 
-        else
-        {
-            i++; // increment time step counter                     
-        }                                
-    }                                  
-}
 
 void clearDisp()
 {
@@ -140,21 +108,69 @@ void getTemperature()
     digitBuffer[0] = (int(tempC)%100)%10;
 }
 
+void change_fan_speed()
+{
+    switch (fan_speed)
+    {
+        case 0:
+            digitalWrite(PIN_FAN_TRIAC_1, LOW);
+            digitalWrite(PIN_FAN_TRIAC_2, LOW);
+            digitalWrite(PIN_FAN_TRIAC_3, LOW);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 1:
+            digitalWrite(PIN_FAN_TRIAC_1, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_2, LOW);
+            digitalWrite(PIN_FAN_TRIAC_3, LOW);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 2:
+            digitalWrite(PIN_FAN_TRIAC_1, LOW);
+            digitalWrite(PIN_FAN_TRIAC_2, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_3, LOW);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 3:
+            digitalWrite(PIN_FAN_TRIAC_1, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_2, LOW);
+            digitalWrite(PIN_FAN_TRIAC_3, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 4:
+            digitalWrite(PIN_FAN_TRIAC_1, LOW);
+            digitalWrite(PIN_FAN_TRIAC_2, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_3, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 5:
+            digitalWrite(PIN_FAN_TRIAC_1, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_2, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_3, HIGH);
+            digitalWrite(PIN_FAN_TRIAC_4, LOW);                
+            break;
+        case 6:
+            digitalWrite(PIN_FAN_TRIAC_1, LOW);
+            digitalWrite(PIN_FAN_TRIAC_2, LOW);
+            digitalWrite(PIN_FAN_TRIAC_3, LOW);
+            digitalWrite(PIN_FAN_TRIAC_4, HIGH);                
+            break;
+    }
+}
+
 void setup()
 {
-    pinMode(AC_pin, OUTPUT);                          // Set the Triac pin as output
-    attachInterrupt(0, zero_cross_detect, RISING);   // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
-
-    // Use the TimerOne Library to attach an interrupt
-    // to the function we use to check to see if it is 
-    // the right time to fire the triac.  This function 
-    // will now run every freqStep in microseconds.                                            
-    Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
-    Timer1.attachInterrupt(dim_check, freqStep);      
-
     sensors.begin();
     Serial.begin(9600);
     irrecv.enableIRIn(); // Starts the receiver
+
+    pinMode(PIN_LIGHT_RELAY, OUTPUT);
+    pinMode(PIN_FAN_RELAY,   OUTPUT);
+
+    pinMode(PIN_LIGHT_TRIAC, OUTPUT);
+    pinMode(PIN_FAN_TRIAC_1, OUTPUT);
+    pinMode(PIN_FAN_TRIAC_2, OUTPUT);
+    pinMode(PIN_FAN_TRIAC_3, OUTPUT);
+    pinMode(PIN_FAN_TRIAC_4, OUTPUT);
 
     for(int k=0;k<4;k++)
     {
@@ -164,6 +180,9 @@ void setup()
     pinMode(latchPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
+
+    digitalWrite(PIN_LIGHT_RELAY, HIGH);
+    digitalWrite(PIN_FAN_RELAY,   HIGH);
 
     getTemperature();
 }
@@ -180,11 +199,10 @@ void loop(){
             Serial.println("Checking if temperature is below 22.0 C ....");
             if (tempC < 2200)
             {
-                fan_speed_trigger = 0;
-            }
-            else
-            {
-                fan_speed_trigger = fan_speed;
+                digitalWrite(PIN_FAN_TRIAC_1, LOW);
+                digitalWrite(PIN_FAN_TRIAC_2, LOW);
+                digitalWrite(PIN_FAN_TRIAC_3, LOW);
+                digitalWrite(PIN_FAN_TRIAC_4, LOW);                
             }
         }
     }
@@ -199,48 +217,53 @@ void loop(){
         {
             case 16738455: /* 0 */
                 fan_speed = 0;
+                change_fan_speed();
                 break;
             case 16724175: /* 1 */
                 fan_speed = 1;
+                change_fan_speed();
                 break;
             case 16718055: /* 2 */
                 fan_speed = 2;
+                change_fan_speed();
                 break;
             case 16743045: /* 3 */
                 fan_speed = 3;
+                change_fan_speed();
                 break;
             case 16716015: /* 4 */
                 fan_speed = 4;
+                change_fan_speed();
                 break;
             case 16726215: /* 5 */
                 fan_speed = 5;
+                change_fan_speed();
                 break;
             case 16734885: /* 6 */
                 fan_speed = 6;
-                break;
-            case 16728765: /* 7 */
-                fan_speed = 7;
-                break;
-            case 16730805: /* 8 */
-                fan_speed = 8;
-                break;
-            case 16732845: /* 9 */
-                fan_speed = 9;
+                change_fan_speed();
                 break;
             case 16748655: /* + */
-                if(fan_speed<9)
+                if(fan_speed<6)
                     fan_speed++;
+                change_fan_speed();
                 break;
             case 16754775: /* - */
                 if(fan_speed>0)
                     fan_speed--;
+                change_fan_speed();
                 break;
-            case 16736925: /* + */
+            case 16736925: /* Mode */
+                digitalWrite(PIN_LIGHT_TRIAC, LOW);
+                break;
+            case 16769565: /* mute */
+                digitalWrite(PIN_LIGHT_TRIAC, HIGH);
+                break;
+            case 16712445: /* rewind */
                 bAutoMode = true;
                 break;
-            case 16769565: /* + */
+            case 16761405: /* forward */
                 bAutoMode = false;
-                fan_speed_trigger = fan_speed;                
                 break;
         }
         irrecv.resume(); // Receives the next value from the button you press
